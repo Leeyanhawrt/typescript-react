c1570b80659de5814095a0f8e158a8b2f0ab2632
Leeyanhawrt <leeyan1108@gmail.com>
2023-08-24 15:08:29 -0400

    typescript with react components and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ToDoList from './components/ToDoList';
 
-function App() {
+const App: React.FC = () => {
+  const todos = [{id: 't1', text:'Finish the course'}];
   return (
     <div className="App">
-     
+     <ToDoList items={todos}/>
     </div>
   );
 }
